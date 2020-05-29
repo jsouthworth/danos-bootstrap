668996b106ab80ad648c8f0b4b2474496ae71f59
John Southworth <john.southworth@gmail.com>
2020-05-29 13:31:33 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module danos-bootstrap
+
+go 1.13
+
+require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/danos/utils v0.0.0-20191119162830-b62d93ee5a7e
+	github.com/go-git/go-git/v5 v5.1.0
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
+	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
+	jsouthworth.net/go/danos-buildpackage v0.0.0-20200529151524-e3f0b2d65e80
+	jsouthworth.net/go/dyn v0.0.0-20190414143850-c77f9cc7c983
+	jsouthworth.net/go/immutable v0.0.0-20200428214457-0cfcea959a4d
+	jsouthworth.net/go/seq v0.0.0-20190918170623-46e5a194a36e // indirect
+	jsouthworth.net/go/transduce v0.0.0-20190413184222-84fc8240a117 // indirect
+	pault.ag/go/debian v0.0.0-20190530135403-b831f604d664
+)
---
+++ main.go
@@ -0,0 +1,303 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/danos/utils/tsort"
+	git "github.com/go-git/go-git/v5"
+	"github.com/google/go-github/github"
+	bpkg "jsouthworth.net/go/danos-buildpackage"
+	"pault.ag/go/debian/control"
+)
+
+var (
+	clone   bool
+	build   bool
+	srcDir  string
+	pkgDir  string
+	logDir  string
+	version string
+)
+
+func resolvePath(in string) string {
+	out, err := filepath.Abs(in)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	return out
+}
+
+type buildError struct {
+	repo string
+	err  error
+}
+
+func (e buildError) Error() string {
+	return fmt.Sprintf("build for %s failed: %s", e.repo, e.err)
+}
+
+type cloneError struct {
+	repo string
+	err  error
+}
+
+func (e cloneError) Error() string {
+	return fmt.Sprintf("clone for %s failed: %s", e.repo, e.err)
+}
+
+type errList []error
+
+func (l errList) Error() string {
+	var buf bytes.Buffer
+	for _, err := range l {
+		fmt.Fprintln(&buf, err)
+	}
+	return buf.String()
+}
+
+func cloneRepos(into string) error {
+	client := github.NewClient(nil)
+
+	opt := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{PerPage: 100},
+	}
+	// get all pages of results
+	var allRepos []*github.Repository
+	ctx := context.Background()
+	for {
+		repos, resp, err := client.Repositories.ListByOrg(ctx,
+			"danos", opt)
+		if err != nil {
+			return err
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	var cloneErrs errList
+	for _, repo := range allRepos {
+		if repo.Archived != nil && *repo.Archived {
+			continue
+		}
+		fmt.Println("Cloning", *repo.Name, "from", *repo.CloneURL)
+		_, err := git.PlainClone(filepath.Join(into, *repo.Name), false,
+			&git.CloneOptions{
+				URL:      *repo.CloneURL,
+				Progress: os.Stdout,
+			})
+		if err != nil {
+			cloneErrs = append(cloneErrs,
+				cloneError{repo: *repo.Name, err: err})
+		}
+	}
+	if len(cloneErrs) != 0 {
+		return cloneErrs
+	}
+	return nil
+}
+
+type repoMetaData struct {
+	ctrlFiles   map[string]*control.Control
+	pack2repo   map[string]string
+	unparseable []string
+}
+
+func enumerateBuildableRepos(from string) repoMetaData {
+	out := repoMetaData{
+		unparseable: []string{},
+		ctrlFiles:   make(map[string]*control.Control),
+		pack2repo:   make(map[string]string),
+	}
+	repos, err := ioutil.ReadDir(from)
+	if err != nil {
+		panic(err)
+	}
+	for _, repo := range repos {
+		path := filepath.Join(from, repo.Name(), "debian", "control")
+		ctrlFile, err := os.Open(path)
+		if err != nil {
+			// this repo does not contain a debian package
+			continue
+		}
+		defer ctrlFile.Close()
+		ctrl, err := control.ParseControl(
+			bufio.NewReader(ctrlFile), path)
+		if err != nil {
+			// if there is a control file but it cannot be parsed
+			// by this tool, we'll attempt to just build it last
+			// the control files should get fixed so this
+			// is unnecessary.
+			out.unparseable = append(out.unparseable, repo.Name())
+			continue
+		}
+		out.ctrlFiles[repo.Name()] = ctrl
+		for _, bin := range ctrl.Binaries {
+			pkgName := strings.TrimSpace(bin.Package)
+			out.pack2repo[pkgName] = repo.Name()
+		}
+	}
+	return out
+}
+
+func determineBuildOrder(repos repoMetaData) []string {
+	depGraph := tsort.New()
+	for repo, ctrl := range repos.ctrlFiles {
+		depGraph.AddVertex(repo)
+		for _, rel := range ctrl.Source.BuildDepends.Relations {
+			for _, pos := range rel.Possibilities {
+				name := strings.TrimSpace(pos.Name)
+				drepo, ok := repos.pack2repo[name]
+				if !ok {
+					// the dependency is not from
+					// a DANOS repository
+					continue
+				}
+				depGraph.AddEdge(repo, drepo)
+			}
+		}
+	}
+
+	sorted, err := depGraph.Sort()
+	if err != nil {
+		panic(err)
+	}
+
+	return append(sorted, repos.unparseable...)
+}
+
+func buildRepo(debDir, baseDir, repo, version string) error {
+	fmt.Println("Building", repo)
+	bldr, err := bpkg.MakeBuilder(
+		bpkg.SourceDirectory(
+			resolvePath(filepath.Join(baseDir, repo))),
+		bpkg.DestinationDirectory(resolvePath(debDir)),
+		bpkg.PreferredPackageDirectory(resolvePath(debDir)),
+		bpkg.Version(version),
+	)
+	if err != nil {
+		return buildError{repo: repo, err: err}
+	}
+	defer bldr.Close()
+	err = bldr.Build()
+	if err != nil {
+		return buildError{repo: repo, err: err}
+	}
+	return nil
+}
+
+func buildRepos(repos []string, logDir, debDir, baseDir, version string) error {
+	var buildErrs errList
+	done := make(chan struct{})
+	interrupt := make(chan os.Signal)
+	signal.Notify(interrupt, os.Interrupt)
+	go func() {
+		for _, repo := range repos {
+			err := teeAndEval(logDir, repo, func() error {
+				return buildRepo(debDir, baseDir, repo, version)
+			})
+			if err != nil {
+				buildErrs = append(buildErrs, err)
+			}
+		}
+		close(done)
+	}()
+	select {
+	case <-done:
+		fmt.Println("finished builds")
+	case <-interrupt:
+		fmt.Println("interrupt received")
+	}
+	if len(buildErrs) != 0 {
+		return buildErrs
+	}
+	return nil
+
+}
+
+func teeAndEval(logdir, repo string, fn func() error) error {
+	stdout := os.Stdout
+	stderr := os.Stderr
+	outr, outw, e := os.Pipe()
+	if e != nil {
+		return e
+	}
+	os.Stdout = outw
+	os.Stderr = outw
+
+	outf, e := os.OpenFile(filepath.Join(logdir, repo+".log"),
+		os.O_RDWR|os.O_CREATE, 0755)
+	if e != nil {
+		return e
+	}
+	defer outf.Close()
+
+	out := io.MultiWriter(stdout, outf)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		io.Copy(out, outr)
+		wg.Done()
+	}()
+
+	rval := fn()
+
+	outw.Close()
+	os.Stdout = stdout
+	os.Stderr = stderr
+	wg.Wait()
+
+	return rval
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	flag.BoolVar(&clone, "clone", false, "Clone all DANOS git repos")
+	flag.BoolVar(&build, "build", false, "Build all cloned repos")
+	flag.StringVar(&srcDir, "src", "src", "source directory")
+	flag.StringVar(&pkgDir, "pkg", "pkg", "package directory")
+	flag.StringVar(&logDir, "log", "log", "log directory")
+	flag.StringVar(&version, "version", "latest", "version of danos to build for")
+}
+
+func main() {
+	flag.Parse()
+	if clone {
+		err := cloneRepos(srcDir)
+		handleError(err)
+	}
+
+	repos := enumerateBuildableRepos(srcDir)
+	buildOrder := determineBuildOrder(repos)
+
+	fmt.Printf("Build order (%d repos): %s\n",
+		len(buildOrder), buildOrder)
+
+	if build {
+		err := os.MkdirAll(logDir, 0777)
+		handleError(err)
+		err = buildRepos(buildOrder, logDir, pkgDir, srcDir, version)
+		handleError(err)
+	}
+}
